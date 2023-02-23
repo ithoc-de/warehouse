28a2a9ee0fe7a52902bc6a69612ec1bb9692f5a6
Oli Hock <oliver.hock@gmail.com>
2023-02-23 15:18:29 +0100

    #6 send user to keycloak api

--- src/main/java/de/ithoc/warehouse/domain/synchronization/SyncService.java
+++ src/main/java/de/ithoc/warehouse/domain/synchronization/SyncService.java
@@ -4,11 +4,15 @@ import de.ithoc.warehouse.external.authprovider.OidcAdminClient;
 import de.ithoc.warehouse.external.authprovider.OidcTokenClient;
 import de.ithoc.warehouse.external.authprovider.schema.token.Token;
 import de.ithoc.warehouse.external.authprovider.schema.users.User;
+import de.ithoc.warehouse.external.authprovider.schema.users.UserInput;
 import de.ithoc.warehouse.external.epages.EpagesClient;
 import de.ithoc.warehouse.external.epages.schema.orders.BillingAddress;
 import de.ithoc.warehouse.external.epages.schema.orders.Item;
 import de.ithoc.warehouse.persistence.*;
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
+import org.keycloak.models.UserModel;
+import org.mapstruct.factory.Mappers;
 import org.springframework.stereotype.Service;
 
 import java.time.*;
@@ -40,20 +44,41 @@ public class SyncService {
         /*
          * Load the last loading timestamp to filter orders that have been delivered since last load.
          */
-        Optional<SyncHistory> previousSyncHistory = syncHistoryRepository.findTopByOrderByTimestampDesc();
-        if(previousSyncHistory.isEmpty()) {
-            String message = "No previous sync history found. Insert at least one record to the database.";
-            log.error(message);
-            throw new RecordNotFoundException(message);
-        }
-        List<Item> filteredItems = epagesClient.orderItems(previousSyncHistory.get().getTimestamp());
-        log.debug("filteredItems: {}", filteredItems);
-
-        // TODO OHO Update warehouse stocks using quantities from new orders.
+        List<Item> filteredItems = getFilteredItems();
 
         /*
          * Get the customers from the orders and check if they need to be added to the authorization server.
          */
+        checkForOrCreateUser(filteredItems);
+
+        // TODO OHO Update warehouse stocks using quantities from new orders.
+
+        /*
+         * This current datetime is used to save it to the loading history. It will be
+         * used for next orders load.
+         */
+        updateSyncHistory();
+    }
+
+
+    void updateSyncHistory() {
+        LocalDateTime now = LocalDateTime.now();
+        ZonedDateTime zonedDateTimeCet = ZonedDateTime.of(now, ZoneId.systemDefault());
+        LocalDateTime utc = zonedDateTimeCet.withZoneSameInstant(ZoneOffset.UTC).toLocalDateTime();
+
+        SyncHistory syncHistory = new SyncHistory();
+        Optional<SyncEntity> syncEntity = syncEntityRepository.findByName("Orders");
+        if (syncEntity.isEmpty()) {
+            String message = "No sync entity 'Orders' found.";
+            log.error(message);
+            throw new RecordNotFoundException(message);
+        }
+        syncHistory.setTimestamp(utc);
+        syncHistoryRepository.save(syncHistory);
+    }
+
+
+    void checkForOrCreateUser(List<Item> filteredItems) {
         filteredItems.forEach(item -> {
             BillingAddress billingAddress = item.getBillingAddress();
             String emailAddress = billingAddress.getEmailAddress();
@@ -62,38 +87,42 @@ public class SyncService {
 
             Token token = oidcTokenClient.token();
             User user = oidcAdminClient.getUserBy(emailAddress, token).orElseGet(() -> {
-                    User newUser = new User();
-                    newUser.setUsername(emailAddress);
-                    newUser.setEmail(emailAddress);
-                    newUser.setFirstName(firstName);
-                    newUser.setLastName(lastName);
-                    newUser.setEmailVerified(Boolean.TRUE);
-                    newUser.setEnabled(Boolean.TRUE);
-                    newUser.setRequiredActions(List.of("Update Password"));
-                    log.debug("New user in authorization server created: {}", newUser);
-
-                    return newUser;
+                String requiredAction = oidcAdminClient.requiredActionAsString(UserModel.RequiredAction.UPDATE_PASSWORD);
+                UserInput userInput = createUserInput(emailAddress, firstName, lastName, requiredAction);
+                oidcAdminClient.postUser(userInput, token);
+                log.debug("userInput: {}:", userInput);
+                OidcUserMapper oidcUserMapper = Mappers.getMapper(OidcUserMapper.class);
+                return oidcUserMapper.toUser(userInput);
             });
             log.debug("user: {}", user);
         });
+    }
 
-        /*
-         * This current datetime is used to save it to the loading history. It will be
-         * used for next orders load.
-         */
-        LocalDateTime now = LocalDateTime.now();
-        ZonedDateTime zonedDateTimeCet = ZonedDateTime.of(now, ZoneId.systemDefault());
-        LocalDateTime utc = zonedDateTimeCet.withZoneSameInstant(ZoneOffset.UTC).toLocalDateTime();
 
-        SyncHistory syncHistory = new SyncHistory();
-        Optional<SyncEntity> syncEntity = syncEntityRepository.findByName("Orders");
-        if(syncEntity.isEmpty()) {
-            String message = "No sync entity 'Orders' found.";
+    List<Item> getFilteredItems() {
+        Optional<SyncHistory> previousSyncHistory = syncHistoryRepository.findTopByOrderByTimestampDesc();
+        if (previousSyncHistory.isEmpty()) {
+            String message = "No previous sync history found. Insert at least one record to the database.";
             log.error(message);
             throw new RecordNotFoundException(message);
         }
-        syncHistory.setTimestamp(utc);
-        syncHistoryRepository.save(syncHistory);
+        List<Item> filteredItems = epagesClient.orderItems(previousSyncHistory.get().getTimestamp());
+        log.debug("filteredItems: {}", filteredItems);
+        return filteredItems;
+    }
+
+
+    @NotNull
+    static UserInput createUserInput(String emailAddress, String firstName, String lastName, String requiredAction) {
+        UserInput userInput = new UserInput();
+        userInput.setUsername(emailAddress);
+        userInput.setEmail(emailAddress);
+        userInput.setFirstName(firstName);
+        userInput.setLastName(lastName);
+        userInput.setEmailVerified(Boolean.TRUE);
+        userInput.setEnabled(Boolean.TRUE);
+        userInput.setRequiredActions(List.of(requiredAction));
+        return userInput;
     }
 
 }
--- src/test/java/de/ithoc/warehouse/domain/synchronization/SyncServiceTest.java
+++ src/test/java/de/ithoc/warehouse/domain/synchronization/SyncServiceTest.java
@@ -5,14 +5,14 @@ import de.ithoc.warehouse.external.authprovider.OidcAdminClient;
 import de.ithoc.warehouse.external.authprovider.OidcTokenClient;
 import de.ithoc.warehouse.external.authprovider.schema.token.Token;
 import de.ithoc.warehouse.external.authprovider.schema.users.User;
+import de.ithoc.warehouse.external.authprovider.schema.users.UserInput;
 import de.ithoc.warehouse.external.epages.EpagesClient;
+import de.ithoc.warehouse.external.epages.schema.orders.Item;
 import de.ithoc.warehouse.external.epages.schema.orders.Orders;
-import de.ithoc.warehouse.persistence.SyncEntity;
-import de.ithoc.warehouse.persistence.SyncEntityRepository;
-import de.ithoc.warehouse.persistence.SyncHistory;
-import de.ithoc.warehouse.persistence.SyncHistoryRepository;
+import de.ithoc.warehouse.persistence.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mapstruct.factory.Mappers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -22,8 +22,11 @@ import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -42,11 +45,100 @@ class SyncServiceTest {
 
     @Mock
     private SyncEntityRepository syncEntityRepository;
+
     @Mock
     private SyncHistoryRepository syncHistoryRepository;
 
     @InjectMocks
     private SyncService syncService;
+
+
+    @Test
+    public void toUserInput() {
+        User user = new User();
+        user.setUsername("user.name@example.com");
+        user.setEmail("user.name@example.com");
+        user.setFirstName("User");
+        user.setLastName("Name");
+        user.setEmailVerified(true);
+        user.setEnabled(Boolean.TRUE);
+        user.setRequiredActions(List.of("UPDATE_PASSWORD"));
+
+        OidcUserMapper oidcUserMapper = Mappers.getMapper(OidcUserMapper.class);
+        UserInput userInput = oidcUserMapper.toUserInput(user);
+
+        assertThat(userInput.getUsername()).isEqualTo("user.name@example.com");
+        assertThat(userInput.getEmail()).isEqualTo("user.name@example.com");
+        assertThat(userInput.getFirstName()).isEqualTo("User");
+        assertThat(userInput.getLastName()).isEqualTo("Name");
+        assertThat(userInput.getEmailVerified()).isEqualTo(true);
+        assertThat(userInput.getEnabled()).isEqualTo(true);
+        assertThat(userInput.getRequiredActions().get(0)).isEqualTo("UPDATE_PASSWORD");
+    }
+
+
+    @Test
+    public void toUser() {
+        UserInput userInput = new UserInput();
+        userInput.setUsername("user.name@example.com");
+        userInput.setEmail("user.name@example.com");
+        userInput.setFirstName("User");
+        userInput.setLastName("Name");
+        userInput.setEmailVerified(true);
+        userInput.setEnabled(Boolean.TRUE);
+        userInput.setRequiredActions(List.of("UPDATE_PASSWORD"));
+
+        OidcUserMapper oidcUserMapper = Mappers.getMapper(OidcUserMapper.class);
+        User user = oidcUserMapper.toUser(userInput);
+
+        assertThat(user.getUsername()).isEqualTo("user.name@example.com");
+        assertThat(user.getEmail()).isEqualTo("user.name@example.com");
+        assertThat(user.getFirstName()).isEqualTo("User");
+        assertThat(user.getLastName()).isEqualTo("Name");
+        assertThat(user.getEmailVerified()).isEqualTo(true);
+        assertThat(user.getEnabled()).isEqualTo(true);
+        assertThat(user.getRequiredActions().get(0)).isEqualTo("UPDATE_PASSWORD");
+    }
+
+
+    @Test
+    public void createUserInput() {
+        UserInput userInput = SyncService.createUserInput("user.name@example.com",
+                "User", "Name", "UPDATE_PASSWORD");
+
+        assertThat(userInput.getUsername()).isEqualTo("user.name@example.com");
+        assertThat(userInput.getEmail()).isEqualTo("user.name@example.com");
+        assertThat(userInput.getFirstName()).isEqualTo("User");
+        assertThat(userInput.getLastName()).isEqualTo("Name");
+        assertThat(userInput.getEmailVerified()).isEqualTo(true);
+        assertThat(userInput.getEnabled()).isEqualTo(true);
+        assertThat(userInput.getRequiredActions().get(0)).isEqualTo("UPDATE_PASSWORD");
+    }
+
+
+    @Test
+    public void getFilteredItems() throws IOException {
+        SyncHistory syncHistory = new SyncHistory();
+        syncHistory.setTimestamp(LocalDateTime.now());
+        when(syncHistoryRepository.findTopByOrderByTimestampDesc()).thenReturn(Optional.of(syncHistory));
+
+        Orders orders = loadTestOrders();
+        List<Item> filteredItemsMock = orders.getItems();
+        when(epagesClient.orderItems(any(LocalDateTime.class))).thenReturn(filteredItemsMock);
+
+        List<Item> filteredItems = syncService.getFilteredItems();
+        assertThat(filteredItems.size()).isGreaterThan(0);
+    }
+
+
+    @Test
+    public void getFilteredItemsNoHistory() {
+        when(syncHistoryRepository.findTopByOrderByTimestampDesc()).thenReturn(Optional.empty());
+
+        assertThrows(RecordNotFoundException.class, () -> {
+            syncService.getFilteredItems();
+        });
+    }
 
 
     @Test
