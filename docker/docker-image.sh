8926f81a6bf213f31c51859a7797295afcc1bfec
Oli Hock <oliver.hock@gmail.com>
2023-03-10 19:49:47 +0100

    create keycloak database on startup

--- docker/docker-image.sh
+++ docker/docker-image.sh
@@ -1,4 +1,5 @@
 docker rmi -f warehouse:latest
-mvn -f pom.xml clean package
+mvn -f pom.xml clean package -DskipTests
+mvn test
 docker build --no-cache --tag warehouse:latest .
 docker images | grep warehouse
