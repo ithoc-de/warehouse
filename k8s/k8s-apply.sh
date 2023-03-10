67bd539498ae721dc60aa9bdbe5f1df6f809fb8e
Oli Hock <oliver.hock@gmail.com>
2023-03-10 21:10:50 +0100

    configure keycloak ingress to access auth admin console

--- k8s/k8s-apply.sh
+++ k8s/k8s-apply.sh
@@ -5,8 +5,8 @@ kubectl apply -f k8s/postgres-service.yaml
 
 kubectl apply -f k8s/keycloak-config.yaml
 kubectl apply -f k8s/keycloak-deployment.yaml
-kubectl apply -f k8s/keycloak-service.yaml
-#kubectl apply -f k8s/keycloak-ingress.yaml
+#kubectl apply -f k8s/keycloak-service.yaml
+kubectl apply -f k8s/keycloak-ingress.yaml
 
 kubectl apply -f k8s/warehouse-config.yaml
 kubectl apply -f k8s/warehouse-deployment.yaml
