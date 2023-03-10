a3beaaf1c2e1d43f4b46e3580cf9ea78740026de
Oli Hock <oliver.hock@gmail.com>
2023-03-11 00:45:24 +0100

    divide databases for keycloak and warehouse to deal with schema creation

--- docker/docker-image.sh
+++ docker/docker-image.sh
@@ -1,5 +1,5 @@
 docker rmi -f warehouse:latest
-mvn -f pom.xml clean package -DskipTests
-mvn test
+mvn clean package -DskipTests
+#mvn test
 docker build --no-cache --tag warehouse:latest .
 docker images | grep warehouse
--- k8s/k8s-apply.sh
+++ k8s/k8s-apply.sh
@@ -1,11 +1,16 @@
-kubectl apply -f k8s/postgres-config.yaml
-kubectl apply -f k8s/postgres-persistence.yaml
-kubectl apply -f k8s/postgres-deployment.yaml
-kubectl apply -f k8s/postgres-service.yaml
+kubectl apply -f k8s/db-keycloak-config.yaml
+kubectl apply -f k8s/db-keycloak-persistence.yaml
+kubectl apply -f k8s/db-keycloak-deployment.yaml
+kubectl apply -f k8s/db-keycloak-service.yaml
+
+kubectl apply -f k8s/db-warehouse-config.yaml
+kubectl apply -f k8s/db-warehouse-persistence.yaml
+kubectl apply -f k8s/db-warehouse-deployment.yaml
+kubectl apply -f k8s/db-warehouse-service.yaml
 
 kubectl apply -f k8s/keycloak-config.yaml
 kubectl apply -f k8s/keycloak-deployment.yaml
-#kubectl apply -f k8s/keycloak-service.yaml
+kubectl apply -f k8s/keycloak-service.yaml
 kubectl apply -f k8s/keycloak-ingress.yaml
 
 kubectl apply -f k8s/warehouse-config.yaml
