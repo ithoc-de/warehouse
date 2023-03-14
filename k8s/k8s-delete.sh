e92a7dfe3b6be481ab5f7e9f527335ab59fe4cf2
Hock <oliver.hock@mecom.de>
2023-03-14 13:59:58 +0100

    configure manifest to fix routing

--- k8s/k8s-delete.sh
+++ k8s/k8s-delete.sh
@@ -2,21 +2,19 @@ kubectl delete -n default service warehouse
 kubectl delete -n default deployment warehouse
 kubectl delete configmap warehouse
 
-kubectl delete ingress keycloak
-kubectl delete -n default service keycloak
-kubectl delete -n default deployment keycloak
-kubectl delete configmap keycloak
+#kubectl delete ingress keycloak
+#kubectl delete -n default service keycloak
+#kubectl delete -n default deployment keycloak
+#kubectl delete configmap keycloak
 
-kubectl delete -n default service db-warehouse
-kubectl delete -n default deployment db-warehouse
-kubectl delete pvc db-warehouse
-kubectl delete pv db-warehouse
-kubectl delete configmap db-warehouse
+#kubectl delete -n default service db-warehouse
+#kubectl delete -n default deployment db-warehouse
+#kubectl delete pvc db-warehouse
+#kubectl delete pv db-warehouse
+#kubectl delete configmap db-warehouse
 
-kubectl delete -n default service db-keycloak
-kubectl delete -n default deployment db-keycloak
-kubectl delete pvc db-keycloak
-kubectl delete pv db-keycloak
-kubectl delete configmap db-keycloak
-
-
+#kubectl delete -n default service db-keycloak
+#kubectl delete -n default deployment db-keycloak
+#kubectl delete pvc db-keycloak
+#kubectl delete pv db-keycloak
+#kubectl delete configmap db-keycloak
